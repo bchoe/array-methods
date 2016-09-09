b1996d60997d5c57eede3a93e1403bd286db9ea2
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-08 15:28:01 -1000

    arrayMethods complete

--- arrayMethods.js
+++ arrayMethods.js
@@ -9,37 +9,38 @@ var mixedWords = ['Dont', 'You', 'Forget', 'About', 'Me'];
 var fruitCollection = ['Apple', 'Scissors', 'Pug', 'Lady Bug', 'Banana', 'Pear'];
 var gemBox = ['Ruby', 'Diamond', 'Diamond', 'Emerald', 'Moonstone', 'Pizza', 'Sneaker', 'Sapphire', 'Baby', 'Jeans', 'Soda Can', 'Boat'];
 var upToTen = [1, 2, 9, 10];
-
 var brownOnly = ['brown', 'brownish', 'ad8fy7iuhj23nrsf', 9813, {}, undefined,  'brown'];
-
 var orderedValues = [4, 5, 6, 7, 8, 9, 10];
-
 var friends = ['Todd', 'Jacoby', 'Miko', 'Joseph', 'Kevin', 'Todd C.'];
-
 var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
 
+
 var nameArray = ['Bryan', 'Choe'];
-
 var restOfMissingNumbers =  genericNumberArray.push(6,7,8,9,10);
-
 var popResult = colors.pop();
-
 var unScrambledWords = scrambledWords.reverse();
-
 var unScramblePn = phoneNumber.reverse();
-
 var nextOrder = orderQueue.shift();
-
 var newNums= mixedNums.sort();
-
 var newWords= mixedWords.sort();
-
 var notFruit = fruitCollection.splice(1,3);
-
 var removeFirstGems= gemBox.splice(5,7, 'Sapphire');
-
 var insertNum= upToTen.splice(2,0,3,4,5,6,7,8);
+var justBrown= brownOnly.splice(1,5,'brown','brown','brown');
+var orderedValuesNewLength= orderedValues.unshift(1,2,3);
+var randomThingsArray= genericNumberArray.concat(colors);
+var updatedOrders= orderQueue.concat([{ takeOut: 'pork'},{ takeOut: 'fries'}]);
+var sentence= scrambledWords.join(' ');
+var myFruits= fruitCollection.join(' + ');
+var favoriteFriends= friends.slice(1,3);
+var owesMoney= friends.slice(3,6);
+var monthNameString= monthNames.toString();
+var favoriteColor= colors.indexOf('Red');
+var favoriteEvenNumber= mixedNums.indexOf(76);
+
+var lastNine= bulkNumbers.lastIndexOf(9);
+var lastFive= bulkNumbers.lastIndexOf(5);
